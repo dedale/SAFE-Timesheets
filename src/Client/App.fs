d3a2bce7b7ecaaae8b0e9c205d9d320387f11793
dedale <ded@laposte.net>
2021-01-08 21:36:18 +0100

    Rename App & Main

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -1,19 +1,39 @@
 module App
 
 open Elmish
-open Elmish.React
+open Thoth.Fetch
 
-#if DEBUG
-open Elmish.Debug
-open Elmish.HMR
-#endif
+open Shared
 
-Program.mkProgram Index.init Index.update Index.view
-#if DEBUG
-|> Program.withConsoleTrace
-#endif
-|> Program.withReactSynchronous "elmish-app"
-#if DEBUG
-|> Program.withDebugger
-#endif
-|> Program.run
+type Model =
+    { Hello: string }
+
+type Msg =
+    | GotHello of string
+
+let init() =
+    let model : Model =
+        { Hello = "" }
+    let getHello() = Fetch.get<unit, string> Route.hello
+    let cmd = Cmd.OfPromise.perform getHello () GotHello
+    model, cmd
+
+let update msg model =
+    match msg with
+    | GotHello hello ->
+        { model with Hello = hello }, Cmd.none
+
+open Feliz
+
+let view model dispatch =
+    Html.div [
+        prop.style [
+            style.textAlign.center
+            style.padding 40
+        ]
+        prop.children [
+            Html.img [ prop.src "favicon.png" ]
+            Html.h1 "Timesheets"
+            Html.h2 model.Hello
+        ]
+    ]
